ce20ce1f03ae70a265c0a0354a6f2a2f334b4733
Yeva22 <y.shimanayev@gmail.com>
2022-10-17 22:05:04 -0400

    Names added to test 1

--- src/OurTest1.java
+++ src/OurTest1.java
@@ -1,6 +1,9 @@
 public class OurTest1 {
     public static void main(String[] args) {
         System.out.println("This is our first collaboration");
+        String names  = "Jenya and Aizhamal";
+        System.out.println("Creators are: " + names);
+
 
     }
 }
